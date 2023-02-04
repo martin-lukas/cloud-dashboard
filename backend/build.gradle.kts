a527a3ba8c87075a40ff82e64b8c3699e18d188e
Martin Lukas <martinlukas24@protonmail.com>
2023-02-04 14:20:06 +0000

    Create basic Spring Boot project

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,15 +1,21 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot").version("3.0.2")
+    id("io.spring.dependency-management").version("1.1.0")
 }
 
 group = "org.lukas"
 version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
